ddc7cc28e8efc8e5b878e8aa8fbc0f50274f2431
Juan Taveira <juantaveira10@gmail.com>
2024-09-19 11:40:23 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,94 @@
+package org.example;
+
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
+import org.apache.beam.runners.dataflow.DataflowRunner;
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.TableRowJsonCoder;
+import org.apache.beam.sdk.io.jdbc.JdbcIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        String driverConnection = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql:///dart_db?"
+                + "cloudSqlInstance=sunny-might-435812-p7:europe-west1:dart"
+                + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory"
+                + "&user=dart_user"
+                + "&password=dart_user";
+        String username = "dart_user";
+        String password = "dart_user";
+        String outputDataset = "dart_test";
+        String outputTable = "dart_table_bq";
+        String projectOutput = "sunny-might-435812-p7";
+        String outputBucket = "dart_test_bucket_123";
+
+        TableSchema schema = new TableSchema().setFields(Arrays.asList(
+                new TableFieldSchema().setName("cob_date_code").setType("STRING"),
+                new TableFieldSchema().setName("bus_desc_l6").setType("STRING"),
+                new TableFieldSchema().setName("scen_code").setType("STRING"),
+                new TableFieldSchema().setName("amnt_code").setType("STRING"),
+                new TableFieldSchema().setName("amount").setType("FLOAT")
+        ));
+
+        DataflowPipelineOptions pipelineOptions = PipelineOptionsFactory.as(DataflowPipelineOptions.class);
+        pipelineOptions.setProject(projectOutput);
+        pipelineOptions.setRegion("europe-west1");
+        pipelineOptions.setTempLocation("gs://" + outputBucket + "/temp");
+        pipelineOptions.setRunner(DataflowRunner.class);
+
+        pipelineOptions.setNumWorkers(1);
+        pipelineOptions.setMaxNumWorkers(1);
+        pipelineOptions.setWorkerMachineType("n1-standard-1");
+        pipelineOptions.setAutoscalingAlgorithm(DataflowPipelineOptions.AutoscalingAlgorithmType.NONE);
+
+        Pipeline p = Pipeline.create(pipelineOptions);
+        p.apply("Read from MySQL", JdbcIO.<TableRow>read()
+                        .withDataSourceConfiguration(JdbcIO.DataSourceConfiguration.create(driverConnection, url)
+                                .withUsername(username)
+                                .withPassword(password))
+                        .withQuery("SELECT * FROM dart_table")
+                        .withRowMapper((JdbcIO.RowMapper<TableRow>) rs -> {
+                            TableRow tableRow = new TableRow();
+                            tableRow.set("cob_date_code", rs.getString("cob_date_code"));
+                            tableRow.set("bus_desc_l6", rs.getString("bus_desc_l6"));
+                            tableRow.set("scen_code", rs.getString("scen_code"));
+                            tableRow.set("amnt_code", rs.getString("amnt_code"));
+                            tableRow.set("amount", rs.getDouble("amount"));
+
+                            return tableRow;
+                        })
+                        .withCoder(TableRowJsonCoder.of())
+                )
+                .apply("Input log", ParDo.of(new DoFn<TableRow, TableRow>() {
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                        TableRow row = c.element();
+                        log.info("Reading line: {}", row);
+                        c.output(row);
+                    }
+                }))
+                .apply("Write into Big Query", BigQueryIO.writeTableRows()
+                        .to(projectOutput + "." + outputDataset + "." + outputTable)
+                        .withSchema(schema)
+                        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                        .withCustomGcsTempLocation(ValueProvider.StaticValueProvider.of(outputBucket))
+                );
+
+        p.run().waitUntilFinish();
+    }
+}
---
+++ src/main/resources/initialData.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS dart_table (
+    bus_desc_l6 VARCHAR(255),
+    cob_date_code VARCHAR(255),
+    amount DOUBLE,
+    amnt_code VARCHAR(255),
+    scen_code VARCHAR(255)
+);
+
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240325', '51764456.8649612', 'MTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240325', '205249893.9591723', 'YTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240325', '745793.7525466', 'DLY', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240325', '205249893.9591723', 'QTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240326', '211009483.4851412', 'YTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240326', '5750799.9642001', 'DLY', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240326', '211009483.4851412', 'QTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240326', '57524046.3909301', 'MTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240327', '3457784.5831295', 'DLY', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240327', '214476115.2027361', 'YTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240327', '214476115.2027361', 'QTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240327', '60990678.108525', 'MTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240328', '62200109.5228896', 'MTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240328', '215685546.6171007', 'YTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240328', '215685546.6171007', 'QTD', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240328', '1174839.7580245', 'DLY', 'Reported');
+Insert into dart_table (BUS_DESC_L6, COB_DATE_CODE, AMOUNT, AMNT_CODE, SCEN_CODE)
+values ('DCM', '20240329', '1', 'DLY', 'Reported');
